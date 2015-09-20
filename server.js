b8573a49e2fd09559abc9fc99195d99fc69bbcec
Jihoon Kim <jhk2020@gmail.com>
2015-09-19 17:24:37 -0700

    website finished

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + "/client"));
 
 app.get('/', function(request, response) {
-	response.sendFile('index.html');
+	response.render('index.html');
 });
 
 app.listen(process.env.PORT || 5000, function() {
